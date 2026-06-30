baf934ce44988ee481009c5c7964d3f4e4e09b7c
Marc Lane <lanem2@tcd.ie>
2026-06-30 20:39:02 +0100

    Prepared start of section 4.2 for cluster

--- reproduction_code/section_4.2/section_4.2.1/scripts/ENCE_elastic_net_C_implementation/elastic_net_R_glue.R
+++ reproduction_code/section_4.2/section_4.2.1/scripts/ENCE_elastic_net_C_implementation/elastic_net_R_glue.R
@@ -7,7 +7,7 @@
 # Version: 5.0
 # Date: 2026-06-19
 
-dyn.load("elastic_net_C_implementation/ENCE_impute.so")
+dyn.load("scripts/ENCE_elastic_net_C_implementation/ENCE_impute.so")
 
 # One synchronous imputation cycle, parallel across stations in C.
 # Returns list(df, lambda, alpha).
